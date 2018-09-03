e43c1ff144a6b10ef6f0714098e3431706505424
hisanari <hisanari.1987@gmail.com>
2018-09-03 02:52:11 +0000

    Hello追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,9 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # vagrantのための設定
+  config.web_console.whitelisted_ips = "0.0.0.0/0"
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
@@ -52,5 +55,6 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # defalut config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.file_watcher = ActiveSupport::FileUpdateChecker
 end
