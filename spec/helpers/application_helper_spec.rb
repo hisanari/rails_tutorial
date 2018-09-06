0bd3aa0c7663a45876c997e437456379b9a21a0c
hisanari <hisanari.1987@gmail.com>
2018-09-06 02:59:14 +0000

    helperテストを修正

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,12 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'applicationHelper', type: :helper do
   include ApplicationHelper
-  
-  it 'full_title_helper 引数無し' do
-    expect(full_title).to eql("Ruby on Rails Tutorial Sample App")
-  end
-
-  it 'full_title_helper 引数あり' do
-    expect(full_title("Home")).to eql("Home | Ruby on Rails Tutorial Sample App")
+  describe 'full_title_helper' do
+    context '引数がない時' do      
+      it 'デフォルトのタイトルを返すこと' do
+        expect(full_title).to eql("Ruby on Rails Tutorial Sample App")
+      end
+    end
+    
+    context '引数がある場合' do
+      it '引数が正しく追加されたタイトルを返すこと' do
+        expect(full_title("Home")).to eql("Home | Ruby on Rails Tutorial Sample App")
+      end
+    end
   end
 end
